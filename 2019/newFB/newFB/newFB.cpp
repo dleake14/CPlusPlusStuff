7804b4bda56c5b6154a683e660a59316d4c69134
David Leake <dleake14@gmail.com>
2019-03-10 19:22:18 -0500

    Added Comments / Cleanup

--- 2019/newFB/newFB/newFB.cpp
+++ 2019/newFB/newFB/newFB.cpp
@@ -4,8 +4,7 @@
 #include <string>
 using namespace std;
 
-int iiii = 0;
-
+//MAIN PRINT METHOD
 void print(string home, string away, int hscrInt, int ascrInt, string qtrNumStr)
 {
 	system("cls");
@@ -18,38 +17,36 @@ void print(string home, string away, int hscrInt, int ascrInt, string qtrNumStr)
 	hscr = to_string(hscrInt);
 	ascr = to_string(ascrInt);
 	qtrNum = qtrNumStr;
-
+	//FORMAT CHECKER FOR HOME LABEL & SCORE
 	if (home.length() % 2 == 0) {
 		hmod = 0;
 	}
-
 	if (hscr.length() % 2 == 0) {
 		hmod2 = 0;
 	}
-
+	//TOP BORDER
 	cout << "Printer\n\n";
 	cout << borders;
-	
+	//FIRST LINE OUTPUT
 	w1 = ((((spac / 2) + 1) + (away.length() / 2)));
 	w2 = ((((spac / 2) + 1) + (qtr.length() / 2)) + (spac - w1));
 	w3 = ((((spac / 2) + hmod) + (home.length() / 2)) + ((spac - w1) + (spac - w2)));
 	cout << setw(w1) << away << setw(w2) << qtr << setw(w3) << home << "\n";
-	
+	//SECOND LINE OUTPUT
 	w12 = ((((spac / 2) + 1) + (ascr.length() / 2)));
 	w22 = ((((spac / 2) + 1) + (qtrNum.length() / 2)) + (spac - w12));
 	w32 = ((((spac / 2) + hmod2) + (hscr.length() / 2)) + ((spac - w12) + (spac - w22)));
 	cout << setw(w12) << ascr << setw(w22) << qtrNum << setw(w32) << hscr;
-	
+	//BOTTOM BORDER
 	cout << "\n";
 	cout << borders;
 }
 
-
+//GET AWAY LABEL
 string getAway() {
 	invalidAway:
 	cout << "\nWho is the away team?" << endl;
 	string away = "";
-	iiii++;
 	cin.clear();
 	getline(cin, away);
 	if (away.length() > 10) {
@@ -59,12 +56,11 @@ string getAway() {
 	return away;
 }
 
-
+//GET HOME LABEL
 string getHome() {
 	invalidHome:
 	cout << "\nWho is the home team?" << endl;
 	string home = "";
-	iiii ++;
 	cin.clear();
 	getline (cin, home);
 	if (home.length() > 10) {
@@ -74,7 +70,7 @@ string getHome() {
 	return home;
 }
 
-
+//GET HOME SCORE
 int getHomeScore() {
 	invalidHScore:
 	cout << "\nHome Score?" << endl;
@@ -87,7 +83,7 @@ int getHomeScore() {
 	return homeScore;
 }
 
-
+//GET AWAY SCORE
 int getAwayScore() {
 	invalidAScore:
 	cout << "\nAway Score" << endl;
@@ -100,7 +96,7 @@ int getAwayScore() {
 	return awayScore;
 }
 
-
+//OVERTIME EXCEPTION
 string ot(int qtrNum) {
 	if (qtrNum > 14) {
 		string qtr = ("OT " + to_string(qtrNum - 4));
@@ -110,7 +106,7 @@ string ot(int qtrNum) {
 	return qtr;
 }
 
-
+//GET QUARTER
 string getQTR() {
 	cout << "\nWhich QTR is it?" << endl;
 	string qtrString = "";
@@ -124,8 +120,7 @@ string getQTR() {
 	return qtrString;
 }
 
-
-
+//MAIN
 int main()
 {
 	cout << "Program Started";
@@ -133,8 +128,6 @@ int main()
 	home = getHome();
 	away = getAway();
 	for (int i = 0; i < 4; ++i) {
-
-
 		int hscr = getHomeScore();
 		int ascr = getAwayScore();
 		string qtr = getQTR();
@@ -143,6 +136,3 @@ int main()
 		cin.get();
 	}
 }
-
-
-
