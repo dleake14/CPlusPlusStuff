2349a74b7ccc9df7ae3750232788597fc81ca0a6
David Leake <dleake14@gmail.com>
2019-03-16 15:57:47 -0500

    C++ Update | 3/16

---
+++ 2019/blank/Blank/Blank.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	cout << "This";
+	const unsigned int count = 10000;
+	string ar[count];
+	string ar2[count];
+	string ar3[count];
+	string ar4[count];
+	string ar5[count];
+	string ar6[count];
+	string ar7[count];
+	string ar8[count];
+	string ar9[count];
+	string ar10[count];
+	for (int i = 0; i < count; i++) {
+		ar[i] = "A WHOLE BUNCH OF TEXT JUST TO TAKE UP MEMORY BECAUSE I'M BORED AND DON'T HAVE INTERNET";
+
+	}
+	cin.get();
+}
---
+++ 2019/firstCLion/cmake-build-debug/CMakeFiles/firstCLion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/firstCLion.dir/main.cpp.obj"
+  "firstCLion.pdb"
+  "firstCLion.exe"
+  "firstCLion.exe.manifest"
+  "firstCLion.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/firstCLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
